878f9eadb4f1cadecf795125a72d81633250761a
Ivan Vivas <ivanvivas@gmail.com>
2015-11-22 17:56:05 -0300

    Anexado alarmas por Telegram Bot. Falta integracion con Heroku

--- src/monitor.java
+++ src/monitor.java
@@ -1,20 +1,31 @@
 import java.sql.*;
+import java.io.*;
+import java.net.*;
+
+
 
 public class monitor {
 	public static void main(String args[]) {
 		Connection conexion = null;
 	    Statement stmt = null;
 	    ResultSet consulta;
-
-	    try {
+	    
+	    int sensorID = 0;
+		int temperatura = 0;
+		int humedad = 0;
+		Timestamp fecha;
+		
+		String sub_url1 = "https://api.telegram.org/";
+		String botToken = "143736528:AAExJNxaLVdjDwvyyorcknZiDf8r65azj8I";
+		String chat_id = "-38984406"; //Chat al grupo SensoresSimGrupo
+		String mensajeT;
+		String mensajeH;
+	    
+		try {
 	    
 	    	//Consultar último registro, 5 veces 
-	    	for (int i=0; i<5; i++) {
-	    		int sensorID;
-	    		int temperatura = 0;
-	    		int humedad = 0;
-	    		Timestamp fecha;
-       	 
+	    	for (int i=0; i<10; i++) {
+	    		
        	 		//Abrir conexion a DB
 	    		Class.forName("org.postgresql.Driver");
 	    		conexion = DriverManager
@@ -34,23 +45,33 @@ public class monitor {
 		           // System.out.println( "sensorid = " + sensorID );
 		           // System.out.println( "Temperatura = " + temperatura );
 		           // System.out.println( "Humedad = " + humedad );
-		           // System.out.println( "Fecha = " + fecha );
+		            System.out.println( "Fecha = " + fecha );
 		           // System.out.println("-----------------------------");
 		         }
 	         
-		         //Cerrar conexion
+		         //Cerrar conexion a DB
 		         consulta.close();
 		         stmt.close();
 		         conexion.commit();
 		         conexion.close();
 		         //System.out.println("Base de datos cerrada");
 		         
-		         //Umbrales
+		         //ALARMAS
 		         if ((temperatura < 40)||(temperatura > 45)) {
-		        	 System.out.println( "ALARMA: Temperatura = " + temperatura );
+		        	//System.out.println( "ALARMA: Temperatura = " + temperatura );
+		        	 mensajeT = "ALARMA: " + 
+		        			 	"Medicion fuera del rango normal. " +
+		        			 	"Temperatura = " + temperatura + " gC," +
+		        			 	" en el sensor " + sensorID;
+		        	 enviarMensajeBot(sub_url1, botToken, chat_id, mensajeT);
 		         }
 		         if ((humedad < 60)||(humedad > 80)) {
-		        	 System.out.println( "ALARMA: Humedad = " + humedad );
+		        	 //System.out.println( "ALARMA: Humedad = " + humedad );
+		        	 mensajeH = "ALARMA: " + 
+		        			 	"Medicion fuera del rango normal. " +
+		        			 	"%20Humedad = " + humedad + " %20%," +
+		        			 	" en el sensor " + sensorID;
+		        	 enviarMensajeBot(sub_url1, botToken, chat_id, mensajeH);
 		         }
 	        
 		         //Pausa por 10 segundos
@@ -65,5 +86,22 @@ public class monitor {
 	    //System.out.println("Registros consultados con exito");
 
 	}//Fin Main
+	
+	public static String enviarMensajeBot(String sub_url1, String botToken, String chat_id, String mensaje) throws Exception {
+		String urlString = sub_url1 + "bot" + botToken + "/sendMessage?" + "chat_id=" + chat_id + "&text=" + mensaje;
+		
+		URL url = new URL(urlString);
+   	 	URLConnection connection = url.openConnection();
+        connection.setDoOutput(true);
 
-}
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        
+        String decodedString;
+        while ((decodedString = in.readLine()) != null) {
+            System.out.println(decodedString);
+        }
+        in.close();
+		return urlString; 
+	}
+
+}//Fin Class Monitor
